27e8fbeb73647e2c512f6ffe0f779cae506f7465
Liad Gross <liadgr@umich.edu>
2026-03-03 20:30:28 +0100

    Milestone 2

---
+++ quizblitz/src/components/ScoreBoard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Game Over</h2>
+    <p>You scored {{ score }} out of 10</p>
+    <button @click="$emit('restart')">Play Again</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScoreBoard',
+  props: {
+    score: Number
+  },
+  emits: ['restart']
+}
+</script>
---
+++ quizblitz/src/components/StartScreen.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>QuizBlitz</h1>
+    <p>Test your web dev knowledge across 10 questions.</p>
+    <button @click="$emit('start')">Play</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StartScreen',
+  emits: ['start']
+}
+</script>
