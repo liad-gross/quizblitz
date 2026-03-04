093e17d02b54ad5fa9f97eb32e50e3c495a6d4bd
Liad Gross <liadgr@umich.edu>
2026-03-04 09:47:33 +0100

    Add views and wire navigation

---
+++ quizblitz/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <StartScreen @start="goToPlay" />
+  </div>
+</template>
+
+<script>
+import StartScreen from '../components/StartScreen.vue'
+
+export default {
+  name: 'HomeView',
+  components: { StartScreen },
+  methods: {
+    goToPlay() {
+      this.$router.push({ name: 'play' })
+    }
+  }
+}
+</script>
