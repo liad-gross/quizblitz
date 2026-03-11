8a71da27fdb766b516b7b056c037496a55a64f78
Liad Gross <liadgr@umich.edu>
2026-03-11 09:35:31 +0100

    ire HomeView to game store

--- quizblitz/src/views/HomeView.vue
+++ quizblitz/src/views/HomeView.vue
@@ -5,14 +5,22 @@
 </template>
 
 <script>
+import { useGameStore } from '../stores/gameStore.js'
 import StartScreen from '../components/StartScreen.vue'
 
 export default {
   name: 'HomeView',
+
   components: { StartScreen },
+
+  setup() {
+    return { store: useGameStore() }
+  },
+
   methods: {
     startGame() {
       sessionStorage.setItem('gameStarted', 'true')
+      this.store.startGame()
       this.$router.push({ name: 'play' })
     }
   }
